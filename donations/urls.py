e7c0172dfea534b1fc50e1269523257dbe6f121d
bilalth <bilalth9@gmail.com>
2023-10-10 11:10:06 +0300

    updated

--- donations/urls.py
+++ donations/urls.py
@@ -1,7 +1,13 @@
-from django.urls import path
-from .views import DonationModelViewSet, DonationRetrieve
+from django.urls import path,include
+from .views import DonationModelViewSet
+from donations.views import DonationModelViewSet
+from rest_framework.routers import DefaultRouter
 
+
+router = DefaultRouter()
+router.register(r"", DonationModelViewSet, basename="donation")
 urlpatterns = [
-    path("", DonationModelViewSet.as_view()),
+    path("", include(router.urls)),
+    # path("", DonationModelViewSet.as_view()),
     # path("<int:pk>", DonationRetrieve.as_view()),
 ]
